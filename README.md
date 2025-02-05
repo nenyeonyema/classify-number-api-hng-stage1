5c8b142db16b9f67f0234c86f8b5ad17b448a112
nenyeonyema <chinenyeonyema1@gmail.com>
2025-02-05 12:16:34 +0100

    number api

---
+++ README.md
@@ -0,0 +1,41 @@
+# A Number Classfiying API for HNG Backend/DevOps Stage 1
+
+## Project Description
+This project is an API that takes a number and classifies them by returning its Mathematical Properties.
+* The Number Value of the Query.
+* Checks if number is a Prime Number.
+* Checks if number is a Perfect Number.
+* Classify its properties if it is an amstrong, odd or even.
+* The Sum of the Digits.
+* Fun fact about the Number.
+
+# API ENDPOINT
+`GET** <your-domain.com>/api/classify-number?number=371`
+
+**RESPONSE**
+```
+    {
+        "number": 371,
+        "is_prime": false,
+        "is_perfect": false,
+        "properties": ["armstrong", "odd"],
+        "digit_sum": 11,  // sum of its digits
+        "fun_fact": "371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371" //gotten from the numbers API
+    }
+```
+## Set Up Instructions
+Clone the repository 
+`git clone https://github.com/nenyeonyema/classify-number-api-hng-stage1`
+
+Navigate to the project directory `cd classify-number-api-hng-stage1`
+
+Install dependencies `npm install`
+
+Run the project locally `node app.js` or `npm run start` or `nodemon app.js`
+
+**Access the API endpoint**
+https://classify-number-api-hng-stage1.vercel.app/
+
+**Hire A Backend NodeJS Developers** https://hng.tech/hire/nodejs-developers
+
+**Hire A DevOps Engineer** https://hng.tech/hire/devops-engineers
---
+++ app.js
@@ -0,0 +1,107 @@
+require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT
+
+app.get("/api/classify-number", (req, res) => {
+
+    const searchQuery = Number(req.query.number);
+
+    const result = {}
+
+    if(isNaN(searchQuery)) {
+        resError = {
+            "number": "alphabet",
+            "error": true
+        }
+        return res.status(400).json(resError);
+    }
+
+    // checks if a number is an integer
+    if(searchQuery % 1 === 0) {
+        result.number = searchQuery;
+
+        // checks if a number is prime
+        let is_prime = true;
+        if(searchQuery <= 1){
+            is_prime = false;
+        } else {
+            for(let i = 2; i < searchQuery; i++){
+                if(searchQuery % i === 0) {
+                    is_prime = false;
+                    break
+                }
+            }
+        }
+        result.is_prime = is_prime;
+
+        // checks if a number is perfect
+        let is_perfect = false;
+        if(searchQuery < 1) {
+            is_perfect = false
+        } else {
+            totalNum = 0
+            for(let i = 1; i < searchQuery; i++){
+                if (searchQuery % i === 0){
+                    
+                    totalNum += i
+                    
+                }
+            }
+            if(searchQuery === totalNum){
+                is_perfect = true;
+            }
+        }
+        result.is_perfect = is_perfect;
+
+        
+        let temp = Math.abs(searchQuery);
+        let sumTemp = 0
+        let digitSum = 0
+        let funFact = ""
+        
+        // Converts the number to a string. splits with a space, 
+        // returns the number as an array and counts the length of the array
+        const numString = temp.toString().split("").map(Number);
+        const count = numString.length;
+
+        // Properties of a Number and Sum of digits
+        let newProperties = []
+        let numArray = []
+
+        numString.forEach(digit => {
+            sumTemp += Math.pow(digit, count);
+            digitSum += digit;
+            numArray.push(`${digit}^${count}`)
+        });
+        
+        // Checks if Number is an Amstrong, Odd or Even.
+        if (sumTemp === Math.abs(searchQuery)){
+            newProperties.push("amstrong")
+            funFact = `${searchQuery}  is an Armstrong number because ${numArray.join(" + ")} = ${temp}`
+        } else{
+            newProperties.push("notAmstrong")
+            funFact = `${searchQuery}  is Not an Armstrong number because ${numArray.join(" + ")} != ${temp}`
+        }
+        if(searchQuery % 2 === 0){
+            newProperties.push("even");
+        } else{
+            newProperties.push("odd")
+        }
+        result.properties = newProperties
+        result.digit_sum = digitSum
+        result.fun_fact = funFact
+    }
+
+    return res.status(200).json(result);
+
+});
+
+app.listen(PORT, () => {
+    console.log(`App Listenning on ${PORT}`)
+})
